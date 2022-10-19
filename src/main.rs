3debcc02d2477b44f4c89d7b33ebcf4e0583d9e5
Sebastian Miasojed <smiasojed.contractor@libertyglobal.com>
2022-10-19 15:30:34 +0200

    removed memory clone

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-
 use std::fmt::{Debug, Display};
 use std::cmp::PartialEq;
 
 #[derive(PartialEq)]
 #[derive(Debug)]
-#[derive(Clone)]
 struct Item<T> {
     x: T,
     address: Option<Box<Item<T>>>
 }
 
-struct List<T: Display + PartialEq + Debug + Clone> {
+struct List<T: Display + PartialEq + Debug> {
     head: Option<Box<Item<T>>>
 }
 
-impl<T: Display + PartialEq + Debug + Clone> List<T> {
+impl<T: Display + PartialEq + Debug> List<T> {
     fn push(&mut self, v: T){
         let el = Item {x: v, address: None};
         let mut e = &mut self.head;
@@ -36,12 +34,26 @@ impl<T: Display + PartialEq + Debug + Clone> List<T> {
         }
     }
 
+    fn delete_all2(&mut self, v: T) {
+        let mut e = &mut self.head;
+        while e.is_some() {
+            let next = &mut e.as_mut().unwrap().address;
+            if next.is_some() {
+                if next.as_ref().unwrap().x == v {
+                    let next_next = next.as_mut().unwrap().address.take();
+                    e.as_mut().unwrap().address = next_next;
+                }
+            }
+            e = &mut e.as_mut().unwrap().address;
+        }
+    }
+
     fn delete_all(&mut self, v: T) {
         let mut e = &mut self.head;
         if let Some(ref mut y) = e {
             //Why it can not be a part of loop (Writing to borrowed variable error)?
             if y.x == v {
-                *e = y.address.clone();
+                *e = y.address.take();
             }
         }
         while let Some(ref mut y) = e {
@@ -50,7 +62,7 @@ impl<T: Display + PartialEq + Debug + Clone> List<T> {
             }*/
             if let Some(ref mut z) = &mut y.address {
                 if z.x == v {
-                    y.address = z.address.clone();
+                    y.address = z.address.take();
                 }
             }
             e = &mut y.address;
@@ -65,11 +77,11 @@ fn main() {
     list.push(3);
     list.push(4);
     list.push(2);
-    list.delete_all(1);
+    list.delete_all2(1);
     list.push(5);
     list.push(6);
     list.push(7);
-    list.delete_all(2);
+    list.delete_all2(2);
     list.delete_all(7);
     list.print();
 }
